c515aeff961a89f0861d050bcb35c6cd7220c70b
HadriBuzz <hadrien.leclerc@mines-ales.org>
2023-11-11 14:39:35 +0100

    sqlite database functions implementation

--- database/source/database_functions.py
+++ database/source/database_functions.py
@@ -1,52 +1,62 @@
 import sqlite3
 import logging
+import pandas as pd
 
-conn = sqlite3.connect("../sqlite_database/missing_masterpieces_db.db")
 
+class database_manager:
+    def __init__(self):
+        self.conn = sqlite3.connect("../../sqlite_database/missing_masterpieces_db.db")
 
-def create_miss_pieces_table():
-    try:
-        conn.execute(
-            """CREATE TABLE MISS_PIECES (ID INT PRIMARY KEY NOT NULL,
-            NAME TEXT NOT NULL,
-            CREATION INT NOT NULL,
-            CREATOR CHAR(50),
-            LOST INT,
-            URL TEXT);"""
+    def create_miss_pieces_table_from_raw_csv(self):
+        try:
+            self.conn.execute(
+                """CREATE TABLE MISS_PIECES (ID INT PRIMARY KEY NOT NULL,
+                NAME TEXT NOT NULL,
+                CREATION INT NOT NULL,
+                AUTHOR CHAR(50),
+                LOST INT NOT NULL,
+                URL TEXT);"""
+            )
+        except Exception as e:
+            logging.info(f"Table already exists: {e}")
+        try:
+            self.add_piece_from_raw_csv()
+            self.conn.commit()
+        except Exception as e:
+            logging.warning(f"Fail to load records: {e}")
+        self.conn.close()
+
+    def add_piece_from_raw_csv(self):
+        df = pd.read_csv("raw_pieces.csv")
+        for index, row in df.iterrows():
+            try:
+                self.conn.execute(
+                    f"INSERT INTO MISS_PIECES (ID,NAME,CREATION,AUTHOR,LOST,URL) VALUES ({row['piece_id']}, \
+                    '{row['name']}', \
+                    {row['created']}, \
+                    '{row['author']}', \
+                    {row['lost']},\
+                    '{row['url']}')"
+                )
+            except Exception as e:
+                logging.info(f"Piece probably exists: {e}")
+
+    def get_all_records(self):
+        cursor = self.conn.execute(
+            f"SELECT id, name, creation, author, lost, url from MISS_PIECES"
         )
+        pieces_dico = list()
+        for row in cursor:
+            pieces_dico.append(
+                {
+                    "piece_id": row[0],
+                    "name": row[1],
+                    "author": row[3],
+                    "creation_date": row[2],
+                    "lost_date": row[4],
+                    "url": row[5],
+                }
+            )
 
-    except Exception as e:
-        logging.warning(f"Table alreayd exists: {e}")
-
-
-def add_piece():
-    try:
-        conn.execute(
-            f"INSERT INTO MISS_PIECES (ID,NAME,CREATION,CREATOR,LOST, URL) VALUES (1, \
-            'Chez Tortoni', \
-            1875, \
-            'Manet', \
-            1990, \
-            'https://upload.wikimedia.org/wikipedia/commons/f/f3/Rembrandt_Christ_in_the_Storm_on_the_Lake_of_Galilee.jpg')"
-        )
-
-        conn.execute(
-            "INSERT INTO MISS_PIECES (ID,NAME,CREATION,CREATOR,LOST, URL) VALUES (2, 'Le Christ dans la tempête sur la mer de Galilée', 1633, 'Rembrandt', 1990, 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Rembrandt_Christ_in_the_Storm_on_the_Lake_of_Galilee.jpg')"
-        )
-    except Exception as e:
-        print(f"Problem while adding piece: {e}")
-
-
-# add_piece()
-conn.commit()
-
-cursor = conn.execute("SELECT id, name, creation, creator, lost from MISS_PIECES")
-print(cursor)
-for row in cursor:
-    print("ID = ", row[0])
-    print("NAME = ", row[1])
-    print("CREATED = ", row[2])
-    print("CREATOR = ", row[3])
-    print("LOST = ", row[4])
-
-conn.close()
+        self.conn.close()
+        return pieces_dico
